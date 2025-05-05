fd39be114915ae7a2270f1f5eef7d1697bf02cbc
hissoune <khalidhissoune962@gmail.com>
2025-05-05 15:09:11 +0100

    refactor: update main App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import Chat from "./chat";
 import BlogPosts from "./BlogPosts";
 import { Provider } from "react-redux";
 import store from "./redux/strore";
+import Register from "./auth/register";
+import ProtectedRoute from "./helpers/protected_routes";
 
 function App() {
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-800 to-gray-900">
+    <div className="h-screen bg-gradient-to-r from-gray-800 to-gray-900">
       <BrowserRouter >
         <Provider store={store}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/chat" element={<Chat />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/chat" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
             <Route path="/blogs" element={<BlogPosts />} />
             
         </Routes>
