2985552a72e3be83929f029bc3cd9a9aeedd36fc
hissoune <khalidhissoune962@gmail.com>
2025-05-06 09:08:30 +0100

    refactor: update main App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,33 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./home";
 import Chat from "./chat";
 import BlogPosts from "./BlogPosts";
-import { Provider } from "react-redux";
-import store from "./redux/strore";
+
 import Register from "./auth/register";
 import ProtectedRoute from "./helpers/protected_routes";
+import Login from "./auth/login";
+import useAppDispatch from "./redux/useAppdispatch";
+import { useEffect } from "react";
+import { loaduserFromCookies } from "./redux/slices/authSlice";
 
 function App() {
-
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    dispatch(loaduserFromCookies())
+  }, [dispatch])
+  
   return (
     <div className="h-screen bg-gradient-to-r from-gray-800 to-gray-900">
       <BrowserRouter >
-        <Provider store={store}>
+        
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/chat" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
             <Route path="/blogs" element={<BlogPosts />} />
             
         </Routes>
-        </Provider>
+       
         
           
         </BrowserRouter>
